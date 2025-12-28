feecf0a4527c4e0b0d8a653ba6a2259f7d7fc5e2
Jeff Emery <jeff.emery@gmail.com>
2025-12-28 16:00:22 -0700

    /help in iframe

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,15 +4,23 @@ import "./style.css";
 import { Header } from "@repo/ui";
 import { BrowserRouter, Route, Routes } from "react-router";
 
-const LandingPage = () => (
+const ChatPage = () => (
   <div>
     <Header title="Envision Technologist Assistant" />
   </div>
 );
 
-const ChatPage = () => (
+const LandingPage = () => (
   <div>
-    <Header title="Chat with the Envision Technologist" />
+    {/* <Header title="Chat with the Envision Technologist" /> */}
+    <iframe id="fullscreen-iframe"
+      src="https://www.chatbase.co/phvfi1BLeWhC3uVaxG8nE/help"
+      title="Example Website"
+      width="600"
+      height="400"
+      loading="lazy" // Optimizes page load by deferring off-screen iframes
+      style={{ border: 'none' }} // Use style object for CSS
+    />
   </div>
 );
 
